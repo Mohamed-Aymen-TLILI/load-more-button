9cc2017d37ec661def6908370bb7bab96df458ca
libra1919 <aymentli@gmail.com>
2021-05-07 23:48:11 +0200

    load-more-button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+  const [Items, setItems] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [page, setPage] = useState(0);
+  const [HasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    loadMoreItems();
+  }, []);
+
+  function loadMoreItems() {
+    setIsFetching(true);
+
+    axios({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/albums",
+      params: { _page: page, _limit: 40 },
+    })
+      .then((res) => {
+        setItems((prevTitles) => {
+          return [...new Set([...prevTitles, ...res.data.map((b) => b.title)])];
+        });
+        setPage((prevPageNumber) => prevPageNumber + 1);
+        setHasMore(res.data.length > 0);
+        setIsFetching(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Items.map((item, index) => {
+        if (Items.length === index + 1) {
+          return (
+            <div key={index}>
+              {item} - <b>last</b>
+            </div>
+          );
+        } else {
+          return <div key={index}>{item}</div>;
+        }
+      })}
+      {isFetching && <p>Fetching items...</p>}
+      {!isFetching && HasMore && (
+        <button onClick={loadMoreItems}>Load more</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
